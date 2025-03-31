a8f3621e475eb454f3343f73147389ce162374eb
Kirill Mashkov <kirill.mashkov@x5.ru>
2025-03-31 22:18:09 +0300

    add parse env var

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -2,16 +2,24 @@ package config
 
 import (
 	"flag"
+	"github.com/caarlos0/env/v6"
 )
 
 type serverConfig struct {
-	Host string
-	Redirect string
+	Host string "env:\"BASE_ADDRESS\""
+	Redirect string "env:\"BASE_URL\""
 }
 
 var Server serverConfig
 
 func init() {
-	flag.StringVar(&Server.Host, "a", "localhost:8080", "server host")
-	flag.StringVar(&Server.Redirect, "b", "http://localhost:8080", "server redirect")
+	env.Parse(&Server)
+
+	if Server.Host == "" {
+		flag.StringVar(&Server.Host, "a", "localhost:8080", "server host")
+	}
+
+	if Server.Redirect == "" {
+		flag.StringVar(&Server.Redirect, "b", "http://localhost:8080", "server redirect")
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kirillmashkov/shortener.git
 go 1.24.1
 
 require (
+	github.com/caarlos0/env/v6 v6.10.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-chi/chi/v5 v5.2.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
