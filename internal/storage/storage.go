27b4bc274f75a7369bd82231c1b66b7f2da3d000
Kirill Mashkov <kirill.mashkov@x5.ru>
2025-04-03 16:02:24 +0300

    fix static test

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -5,8 +5,8 @@ import (
 )
 
 type StoreURL interface {
-	AddUrl(url string, keyURL string)
-	GetUrl() (string, bool)
+	AddURL(url string, keyURL string)
+	GetURL() (string, bool)
 }
 
 type StoreURLMap struct {
@@ -20,13 +20,13 @@ func NewStoreMap() *StoreURLMap {
 	return &storeMap
 }
 
-func (storeMap StoreURLMap) AddUrl(url string, keyURL string) {
+func (storeMap *StoreURLMap) AddUrl(url string, keyURL string) {
 	storeMap.Lock()
 	storeMap.urls[keyURL] = url
 	storeMap.Unlock()
 }
 
-func (storeMap StoreURLMap) GetUrl(keyURL string) (string, bool) {
+func (storeMap *StoreURLMap) GetUrl(keyURL string) (string, bool) {
 	storeMap.RLock()
 	url, exist := storeMap.urls[keyURL]
 	storeMap.RUnlock()
