53a34cb202abe36be9d9425cfd6084d5a518ce78
Kirill Mashkov <kirill.mashkov@x5.ru>
2025-07-20 18:06:01 +0300

    add bench

---
+++ internal/service/service_test.go
@@ -0,0 +1,71 @@
+package service
+
+import (
+	"log"
+	"math/rand/v2"
+	"os"
+	"path"
+	"runtime"
+	"strconv"
+	"testing"
+
+	"github.com/kirillmashkov/shortener.git/internal/config"
+	"github.com/kirillmashkov/shortener.git/internal/storage/database"
+	"go.uber.org/zap"
+)
+
+var ServerConf config.ServerConfig
+var Log *zap.Logger = zap.NewNop()
+var Database *database.Database
+var RepositoryShortURL *database.RepositoryShortURL
+var ServiceShort *Service
+const originalURLPrefix = "http://www.yandex.ru/"
+
+func Init() {
+	config.InitServerConf(&ServerConf, Log)
+	Database = database.New(&ServerConf, Log)
+	
+	if err := Database.Open(); err != nil {
+		Log.Error("Error open database", zap.Error(err))
+		panic(err)
+	}
+
+	if err := Database.Migrate(); err != nil {
+		Log.Error("Error migrate", zap.Error(err))
+		panic(err)
+	}
+	RepositoryShortURL = database.NewRepositoryShortURL(Database, Log)
+	ServiceShort = New(RepositoryShortURL, ServerConf, Log)
+}
+
+func Finish() {
+	errClose := Database.Close()
+	if errClose != nil {
+		Log.Error("Error close connection db", zap.Error(errClose))
+	}
+}
+
+func changeWorkingDir() {
+	_, filename, _, _ := runtime.Caller(0)
+	dir := path.Join(path.Dir(filename), "../../")
+	err := os.Chdir(dir)
+  	if err != nil {
+		Log.Error("Error change working dir", zap.Error(err))
+    	panic(err)
+ 	}
+}
+
+func BenchmarkPostHandler(b *testing.B) {
+	changeWorkingDir()
+	Init()
+	
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	strconv.Itoa(rand.IntN(100000))
+	for i := 0; i < b.N; i++ {
+		ServiceShort.ProcessURL(b.Context(), originalURLPrefix + strconv.Itoa(rand.IntN(100000)), 1)
+	}
+
+	Finish()
+}
+
